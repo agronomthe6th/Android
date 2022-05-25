49862bf70a7c9dc8a03545d1d701b1ddf1a3fa41
agronomthe6th <victor020100@gmail.com>
2022-05-25 21:08:24 +0300

    fix

--- Practice5/camera/src/main/java/ru/mirea/lazarev/camera/MainActivity.java
+++ Practice5/camera/src/main/java/ru/mirea/lazarev/camera/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
@@ -34,17 +35,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         imageView = findViewById(R.id.imageView);
-        int cameraPermissionStatus =
-                ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
-        int storagePermissionStatus =
-                ContextCompat.checkSelfPermission(this,
-                        Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        if (cameraPermissionStatus == PackageManager.PERMISSION_GRANTED &&
-                storagePermissionStatus == PackageManager.PERMISSION_GRANTED) {
+        // Выполняется проверка на наличие разрешений на использование камеры и запись в
+        int cameraPermissionStatus = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        int storagePermissionStatus = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (cameraPermissionStatus == PackageManager.PERMISSION_GRANTED && storagePermissionStatus == PackageManager.PERMISSION_GRANTED) {
             isWork = true;
         } else {
-            ActivityCompat.requestPermissions(this, new String[]
-                            {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE},
+            // Выполняется запрос к пользователь на получение необходимых разрешений
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE},
                     REQUEST_CODE_PERMISSION_CAMERA);
         }
     }
@@ -52,14 +50,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        // Если приложение камера вернула RESULT_OK, то производится установка изображению imageView
         if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
             imageView.setImageURI(imageUri);
         }
     }
-
+    @SuppressLint("QueryPermissionsNeeded")
     public void imageViewOnClick(View view) {
         Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
+        // проверка на наличие разрешений для камеры
         if (cameraIntent.resolveActivity(getPackageManager()) != null && isWork) {
             File photoFile = null;
             try {
@@ -67,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
+            // генерирование пути к файлу на основе authorities
             String authorities = getApplicationContext().getPackageName() + ".fileprovider";
             assert photoFile != null;
             imageUri = FileProvider.getUriForFile(this, authorities, photoFile);
@@ -85,13 +84,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        if (requestCode == REQUEST_CODE_PERMISSION_CAMERA) {
-            isWork = grantResults.length > 0
-                    && grantResults[0] == PackageManager.PERMISSION_GRANTED;
-        }
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    // производится проверка полученного результата от пользователя на запрос разрешения Camera
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE_PERMISSION_CAMERA) {
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // permission granted
+                isWork = true;
+            } else {
+                isWork = false;
+            }
+        }
     }
 }
